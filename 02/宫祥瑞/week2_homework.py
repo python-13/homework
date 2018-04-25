12fd47e512ce242c953b3b7f963d0772b420c458
cyrus1900 <16621046739@163.com>
2018-04-25 17:09:17 +0800

    第二次作业提交的目录

--- 02/宫祥瑞/week2_homework.py
+++ 02/宫祥瑞/week2_homework.py
@@ -5,22 +5,22 @@ tel = []
 passwd = []
 
 flag = 0
-flag1 = 1
 flag2 = 1
 
-def verify():
+def verify(src=user):
     global flag
     while True:
         if flag:
             flag = 0
             break
-        name = input('please input user')
-        for k, v in user.items():
+        name = input('please input user').strip()
+        for k, v in src.items():
             if k == name:
                 passwd = v.get('passwd')
-                password = input('please input password')
+                password = input('please input password').strip()
                 if passwd == password:
                     flag = 1
+                    print('welcome user:{} password:{} log in'.format(name,'*'*len(passwd)))
                     break
                 else:
                     print('password is wrong')
@@ -31,34 +31,35 @@ def verify():
 
 
 # print(user)
-def add():
-    mes = input('please input name:age:telephone:password')
+def add(src=user):
+    mes = input('please input name:age:telephone:password').strip()
     lst = mes.split(':')
-    #print(lst)
-    user.update({lst[0]:{'age': lst[1],'tel': lst[2],'passwd':lst[3]}})
+    src.update({lst[0]:{'age': lst[1],'tel': lst[2],'passwd':lst[3]}})
     print('add sucess user={}\tage={}\ttel={}\tpassword={}'.format(lst[0],lst[1],lst[2],'*'*len(lst[3])))
 
-def delete(name):
-
-    user.pop(name)
+def delete(name,src=user):
+    src.pop(name)
     print('{} is delete.'.format(name))
 
 
-def update(name):
-
-    mes = input('please input age:telephone:password')
+def update(name,src=user):
+    mes = input('please input age:telephone:password').strip()
     lst = mes.split(':')
-    user.update({name: {'age': lst[0], 'tel': lst[1],'passwd':lst[2]}})
+    src.update({name: {'age': lst[0], 'tel': lst[1],'passwd':lst[2]}})
     print('update sucess age={}\ttel={}\tpassword={}'.format( lst[0], lst[1], '*' * len(lst[2])))
 
-#list重大漏洞待解决
-def lst(type):
+
+def lst(src=user):
     # sorted
-    global flag1
-    if flag1:
-        flag1 = 0
-        for k, v in user.items():
+        type = input('please input type in user age tel').strip()
+        def _list(n):
+            usr[n], usr[n + 1] = usr[n + 1], usr[n]
+            age[n], age[n + 1] = age[n + 1], age[n]
+            tel[n], tel[n + 1] = tel[n + 1], tel[n]
+            passwd[j], passwd[j + 1] = passwd[j + 1], passwd[j]
+        for k, v in src.items():
             usr.append(k)
+            #age,tel,passwd = *user[k]
             for k1, v1 in v.items():
                 if k1 == 'age':
                     age.append(v1)
@@ -69,34 +70,27 @@ def lst(type):
 
         for i in range(len(usr)):
             for j in range(len(usr)-i-1):
-                if type == 'user':
+                if type == 'user'or type == '':
                     if usr[j] > usr[j+1]:
-                        usr[j],usr[j+1] = usr[j+1],usr[j]
-                        age[j],age[j+1] = age[j+1],age[j]
-                        tel[j],tel[j+1] = tel[j+1],tel[j]
-                        passwd[j],passwd[j+1] = passwd[j+1],passwd[j]
+                        _list(j)
                 elif type == 'age':
                     if age[j] > age[j+1]:
-                        usr[j],usr[j+1] = usr[j+1],usr[j]
-                        age[j],age[j+1] = age[j+1],age[j]
-                        tel[j],tel[j+1] = tel[j+1],tel[j]
-                        passwd[j], passwd[j + 1] = passwd[j + 1], passwd[j]
+                        _list(j)
                 elif type == 'tel':
                     if tel[j] > tel[j+1]:
-                        usr[j],usr[j+1] = usr[j+1],usr[j]
-                        age[j],age[j+1] = age[j+1],age[j]
-                        tel[j],tel[j+1] = tel[j+1],tel[j]
-                        passwd[j], passwd[j + 1] = passwd[j + 1], passwd[j]
+                        list(j)
                 else:
                     print('type wrong')
                     return -2
-    for n in range(len(usr)):
-        print('user={}\tage={}\ttel={}\tpassword={}'.format(usr[n],age[n],tel[n],'*'*len(passwd[n])))
+        for n in range(len(usr)):
+            print('user={}\tage={}\ttel={}\tpassword={}'.format(usr[n],age[n],tel[n],'*'*len(passwd[n])))
+        usr.clear()
+        age.clear()
+        tel.clear()
 
-
-def find():
-    usr = input('please input a name:')
-    for k, v in user.items():
+def find(src=user):
+    usr = input('please input a name:').strip()
+    for k, v in src.items():
         if k == usr:
             passwd =  v.get('passwd')
             print('age={}\ntel={}\npassword={}'.format(v.get('age'),v.get('tel'),'*' * len(passwd)))
@@ -105,11 +99,11 @@ def find():
 
 
 def main():
-    global flag2
+    global user,flag2
     if flag2:
        flag2 = 0
        verify()
-    opt = input("please input options in 'add delete update find list exit'")
+    opt = input("please input options in 'add delete update find list exit'").strip()
     if opt == 'add':
         add()
         main()
@@ -125,8 +119,7 @@ def main():
         find()
         main()
     elif opt == 'list':
-        type = input('please input type in user age tel')
-        lst(type)
+        lst()
         main()
     elif opt == 'exit':
         return 0
