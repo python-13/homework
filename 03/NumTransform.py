10b3514cd38511f5f243f5fbec3c7f372807eb8a
cuizj923 <361686156@qq.com>
2018-05-06 22:21:55 +0800

    第三次作业完成

---
+++ 03/NumTransform.py
@@ -0,0 +1,41 @@
+import re
+regex = re.compile("['零仟','零佰','零拾']")
+ChinaNumData = {1: '壹', 2: '贰', 3: '叁', 4: '肆', 5: '伍', 6: '陆', 7: '柒', 8: '捌', 9: '玖', 0: '零'}
+Unit = ['元', '拾', '佰', '仟', '万']
+
+def NumTrans(UserInput):
+    """docstring"""
+    Data = UserInput[::-1]
+    Transform = ''
+    count = 0
+    if len(UserInput) == 1:
+        Transform = ChinaNumData[int(UserInput)] + '元整'
+    else:
+        for i in Data:
+            Transform = Transform + Unit[Data.index(i,count)] + ChinaNumData[int(i)]
+            count += 1
+
+    Transform=Transform[::-1]+'元整'
+    print(Transform)
+    for i in ['零仟','零佰','零拾',]:
+        Transform = re.sub(i,'零',Transform)
+        Transform = re.sub(i, '元', Transform)
+
+    Transform = re.sub('零+','零',Transform)
+    Transform = re.sub('零元', '', Transform)
+    print(Transform)
+
+
+
+def main():
+    """docstring"""
+    while 1:
+        UserInput = input('请输入小于99999的数字')
+        while 1:
+            if len(UserInput) <=5:
+                NumTrans(UserInput)
+                break
+            else:
+                print('输入的数字赶出范围')
+                break
+main()
