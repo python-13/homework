7d9b1432093b2db625a214b1a2d4ab91ced09e29
wang7672 <18668247279@163.com>
2018-05-29 21:43:28 +0800

    week3 work

--- 03/王文峰/homework_week3.py
+++ 03/王文峰/homework_week3.py
@@ -1,18 +1,19 @@
 
 """将阿拉伯数字表示的一串整数(小于1万)转换成标准的中文货币表达式，单位为元:比如“1234”转化为"壹仟贰佰叁拾肆元",“1001”转化成为"壹仟零壹元",数字的中文对应：零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖","拾", "佰", "仟", "万"，请试着实验一下"""
-###主管给的建议直接匹配写
 import re
 word_figure = "零壹贰叁肆伍陆柒捌玖"
 units = "元拾佰千万拾佰千亿拾佰千"
+money = []
+
 while True:
     nums = input(">>>Enter a number less than 6 digits from that number").lstrip("0").strip()
     if nums.isdigit() and len(nums) < 11:
         break
     else:
         print("please enter a number less than 11 digits from that number!!!")
-money = []
 nums = list(nums)
 length = len(nums)
+
 for i in range(length):
      num =  int(nums[i])
      money.append(word_figure[num])
